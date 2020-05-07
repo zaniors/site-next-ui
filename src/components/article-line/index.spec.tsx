71d101f6db389b0efd62935c1cbd987e18cf39bc
ZharyZhang <2590000714@qq.com>
2020-05-08 01:21:09 +0800

    article-line.test.tsx

---
+++ src/components/article-line/index.spec.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ArticleLine from './ArticleLine';
+import { render } from '@testing-library/react';
+import ArticleLineItem from './Item';
+
+describe('测试ArticleLine组件', () => {
+  it('测试初始组件带有List数据', () => {
+    const list = [
+      {
+        id: 0,
+        title: '我是第1条测试数据'
+      },
+      {
+        id: 1,
+        title: '我是第2条测试数据'
+      },
+    ];
+    const { getByTestId } = render(<ArticleLine list={list} />);
+    const alWrap = getByTestId('test-al');
+    expect(alWrap).toHaveClass('zan-al');
+    expect(alWrap.childElementCount).toEqual(list.length);
+  });
+
+  it('测试初始组件嵌套ArticleLineItem子组件', () => {
+    const generateAL = () => {
+      return (
+        <ArticleLine>
+          <ArticleLineItem id={0} title="我是第1条测试数据" />
+          <ArticleLineItem id={0} title="我是第2条测试数据" />
+        </ArticleLine>
+      );
+    }
+
+    const { getByTestId } = render(generateAL());
+    const alWrap = getByTestId('test-al');
+    expect(alWrap).toHaveClass('zan-al');
+    expect(alWrap.childElementCount).toEqual(2);
+  });
+});
