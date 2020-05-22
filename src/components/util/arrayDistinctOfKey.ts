6614a0c5100da5e01b47257253a3eeda2041e49f
zhangxu <2590000714@qq.com>
2020-05-22 19:01:57 +0800

    arrayDistinctOfKey.ts

---
+++ src/components/util/arrayDistinctOfKey.ts
@@ -0,0 +1,22 @@
+
+type Skip<T, K extends keyof any> = Partial<Pick<T, Exclude<keyof T, K>>> & Omit<T, K>;
+
+/**
+ * 对数组的指定key的value进行去重，并且只对相邻有效
+ * [{a: 1}, {a: 1}, {a: 2}, {a: 3}, {a: 1}] => [{a: 1}, {a: 2}, {a: 3}, {a: 1}]
+ * @param arr 数组数据源
+ * @param key 指定key
+ */
+export const arrayDistinctOfKey = <T>(arr: Array<T>, key: string): Array<T> => {
+  return arr
+}
+
+export default arrayDistinctOfKey;
+
+interface A {
+  a: string;
+  b: string;
+}
+
+function A(a: Extract<A, 'a'>) {
+}
