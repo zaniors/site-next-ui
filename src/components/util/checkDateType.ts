9ad5c65779430eaa4d73321db2597e23635c3d7d
zhangxu <2590000714@qq.com>
2020-05-22 19:02:07 +0800

    添加注释

--- src/components/util/checkDateType.ts
+++ src/components/util/checkDateType.ts
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param date 可以转化为时间的输入源，比如1589354703078、2020-05-13或者Date对象
+ * @returns 如果能够将输入源转为Date对象，则返回，反之返回false
+ */
 export const transDateType = (date: string | number | Date): Date | boolean => {
   const d = date instanceof Date ? date : new Date(date);
   return !isNaN(d.getTime()) && d ;
